69f66026e370963444dfa7ea251f3d5f5610dbbe
HqmJoker <44420292+HqmJoker@users.noreply.github.com>
2020-11-17 14:04:04 +0800

    移除多余判断条件

--- maxProfit.js
+++ maxProfit.js
@@ -22,7 +22,7 @@ var maxProfit = function(prices) {
     for(let i=0; i<prices.length; i++){
         if(prices[i] < prices[i+1]){
             for(let j=i+1; j<prices.length; j++){
-                if(prices[i]<prices[j] && prices[j]-prices[i]>profit){
+                if(prices[j]-prices[i]>profit){
                     profit = prices[j]-prices[i];
                 }
             }
