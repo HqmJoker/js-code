4929b1c69cbd112a8cfa76b1b5dbb7f4a9802c33
HqmJoker <44420292+HqmJoker@users.noreply.github.com>
2020-12-04 17:55:35 +0800

    四数之和（解决超时，可优化）

--- fourSumCount .js
+++ fourSumCount .js
@@ -6,20 +6,6 @@
  * @return {number}
  */
 var fourSumCount = function(A, B, C, D) {
-    // let result = 0;
-    // let len = A.length;
-    // for(let i=0; i<len; i++){
-    //     for(let j=0; j<len; j++){
-    //         for(let k=0; k<len; k++){
-    //             for(let l=0; l<len; l++){
-    //                 if( !(A[i] + B[j] + C[k] + D[l]) ){
-    //                     result ++;
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-    // return result;
     let result = 0;
     let len = A.length;
     let E = {};
@@ -28,10 +14,10 @@ var fourSumCount = function(A, B, C, D) {
         for(let j=0; j<len; j++){
             let numE = A[i] + B[j]
             let numF = C[i] + D[j]
-            if(!E[numE]){
-                E[numE] = 1;
+            if(!E[-numE]){
+                E[-numE] = 1;
             }else{
-                E[numE]++;
+                E[-numE]++;
             }
 
             if(!F[numF]){
@@ -41,12 +27,47 @@ var fourSumCount = function(A, B, C, D) {
             }
         }
     }
+    console.log(E,F)
     for(let itemE in E){
-        for(let itemF in F){
-            if( !(itemE + itemF) ){
-                result += E[itemE] + F[itemF]
-            }
+        if( F[itemE] ){
+            result += F[itemE] * E[itemE]
         }
     }
     return result;
 };
+
+// var fourSumCount = function(A, B, C, D) {
+//     let result = 0;
+//     let len = A.length;
+//     let E = {};
+//     let F = {};
+//     for(let i=0; i<len; i++){
+//         for(let j=0; j<len; j++){
+//             let numE = A[i] + B[j]
+//             let numF = C[i] + D[j]
+
+//             if(!E[numE]){
+//                 E[numE] = 1;
+//             }else{
+//                 E[numE]++;
+//             }
+
+//             if(!F[numF]){
+//                 F[numF] = 1;
+//             }else{
+//                 F[numF]++;
+//             }
+//             console.log(numE, numF)
+//             if(E[-numF]){
+//                 result += E[-numF];
+//             }
+
+//             if(F[-numE]){
+//                 result += E[-numF];
+//             }
+//         }
+        
+//     }
+//     console.log(E, F)
+//     return result;
+// };
