a204730ae748d38becc9b0f4a35a4d4ba748df73
HqmJoker <44420292+HqmJoker@users.noreply.github.com>
2020-12-21 09:53:01 +0800

    s-746-最小花费爬楼梯，添加注释，优化空间复杂度o(n)->n(1)

--- minCostClimbingStairs.js
+++ minCostClimbingStairs.js
@@ -2,12 +2,17 @@
  * @param {number[]} cost
  * @return {number}
  * 解题思路：
- *     f(n) = min(f(n-1), f(n-2))  每一次的结果均为前一次可到达结果的最小值      
+ *   当 n < 2 
+ *     f(0) = 0
+ *     f(1) = min(value(0), value(1))
+ *   当 n >= 2
+ *     f(n) = min(f(n-1), f(n-2)+value(n-2))  每一次的结果均为前一次可到达结果的最小值      
  */
 var minCostClimbingStairs = function(cost) {
-    let result = [cost[0], cost[1]];
+    let cost0 = cost[0];
+    let cost1 = cost[1];
     for(let i=2; i<cost.length; i++){
-        result[i] = Math.min(result[i-1], result[i-2]) + cost[i];
+        [ cost0, cost1 ] = [ cost1 , Math.min(cost0, cost1) + cost[i] ]  // 只保存前两级楼梯的最小花费值
     }
-    return Math.min(result[cost.length-1], result[cost.length-2]);
+    return Math.min(cost0, cost1);
 };
